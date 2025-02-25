c956ff37b8f85c8b8feb7459033cead99d73b533
arxsyy <marsyfernanda@gmail.com>
2025-02-25 23:19:48 +0700

    seed and migration

--- database/migrations/2025_02_24_043849_create_m_kategori_table.php
+++ database/migrations/2025_02_24_043849_create_m_kategori_table.php
@@ -4,24 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
+            $table->id('kategori_id');
+            $table->string('kategori_kode', 10)->after('kategori_id')->unique();
+            $table->string('kategori_nama', 100);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('m_kategori');
     }
 };
+
--- database/migrations/2025_02_24_043959_create_m_supplier_table.php
+++ database/migrations/2025_02_24_043959_create_m_supplier_table.php
@@ -4,24 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('m_supplier', function (Blueprint $table) {
-            $table->id();
+            $table->id('supplier_id');
+            $table->string('supplier_kode', 10)->unique();
+            $table->string('supplier_nama', 100);
+            $table->string('supplier_alamat', 255)->nullable();
+            $table->string('supplier_telepon', 15)->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('m_supplier');
     }
 };
